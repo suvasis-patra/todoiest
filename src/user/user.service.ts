512d817f42df431e4d5025cfe6005cc44b734869
suvasis-patra <suvasis02@kgpian.iitkgp.ac.in>
2025-10-07 23:33:07 +0530

    add functions to the user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({ data });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { data, where } = params;
+    return this.prisma.user.update({ where, data });
+  }
+
+  async getUserById(id: string): Promise<User | null> {
+    return this.prisma.user.findFirst({ where: { id } });
+  }
+
+  async getUserByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findFirst({ where: { email } });
+  }
+}
